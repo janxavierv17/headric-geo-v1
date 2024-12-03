0b852fa217ef633881d18f8f6005e03cb5456fe6
Jan Xavier Virgen <54022069+janxavierv17@users.noreply.github.com>
2024-12-04 00:02:51 +1100

    custom command to ensure db is ready

---
+++ backend/core/management/commands/wait_for_db.py
@@ -0,0 +1,26 @@
+import time
+from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+from psycopg2 import OperationalError as Psycopg2OpError
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        # Initial log message indicating the start of the process
+        self.stdout.write("Waiting for database to be ready...")
+
+        db_up = False
+        retry = 1
+        while not db_up:
+            try:
+                # Check if the database is available
+                self.check(databases=["default"])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                # Log retry attempts with more detail
+                self.stdout.write(f"DB unavailable. Retry attempt {retry}...")
+                retry += 1
+                time.sleep(1)
+
+        # Log a success message when the database is available
+        self.stdout.write(self.style.SUCCESS("=== Database is available ==="))
