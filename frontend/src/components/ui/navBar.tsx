d722146b8ac66d2e442a23663d4cb0f205da0d44
Jan Xavier Virgen <54022069+janxavierv17@users.noreply.github.com>
2024-12-16 23:33:35 +1100

    navigation bar ui

---
+++ frontend/src/components/ui/navBar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "./button";
+import { HomeIcon, UserIcon } from "@heroicons/react/24/outline";
+import { usePathname } from "next/navigation";
+
+const links = { home: "/", apartment: "/apartment", login: "/login" } as const;
+type allowedUrls = (typeof links)[keyof typeof links];
+
+export const NavBar = () => {
+	const pathname = usePathname();
+	const isActive = (url: allowedUrls): boolean => pathname === url;
+
+	return (
+		<nav className="h-20 pl-8 pr-8">
+			<ul className="flex justify-between items-center h-full">
+				<li>
+					<Link href="/">APT.</Link>
+				</li>
+				<div className="flex justify-between items-center gap-3">
+					<li>
+						<Button
+							className={`hover:bg-emerald-600 ${
+								isActive("/apartment") ? "bg-emerald-700 text-white" : ""
+							} focus:outline-none focus:ring focus:ring-emerald-300`}
+						>
+							<Link href="/apartment" className="flex items-center gap-2">
+								<HomeIcon />
+								Add Apartment
+							</Link>
+						</Button>
+					</li>
+					<li>
+						<Button
+							className={`hover:bg-emerald-600 ${
+								isActive("/login") ? "bg-emerald-700 text-white" : ""
+							} focus:outline-none focus:ring focus:ring-emerald-300`}
+						>
+							<Link href="/login" className="flex items-center gap-2">
+								<UserIcon />
+								Log in
+							</Link>
+						</Button>
+					</li>
+				</div>
+			</ul>
+		</nav>
+	);
+};
